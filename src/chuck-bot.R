d8a3ce6a27474537c16fe4decac50150c8db3a0c
Ryan Elmore <rtelmore@gmail.com>
2018-12-13 12:53:10 -0700

    tweeter bot stuff

--- src/chuck-bot.R
+++ src/chuck-bot.R
@@ -6,27 +6,25 @@
 library(markovifyR)
 library(tidyverse)
 
-fakts <- filter(chuck_df, 
-                is_fakt == 1) %>%
-  select(text) %>%
+fakts <- filter(chuck_clean_df, is_fakt == 1) %>%
+  select(new_text) %>%
   .[[1]]
 
-options(width=200)
-markov_model <-
-  generate_markovify_model(
-    input_text = fakts,
-    markov_state_size = 2L,
-    max_overlap_total = 15,
-    max_overlap_ratio = .7
+markov_model <- generate_markovify_model(
+  input_text = fakts,
+  markov_state_size = 2L,
+  max_overlap_total = 15,
+  max_overlap_ratio = .65
   )
+
 
 markovify_text(
   markov_model = markov_model,
   maximum_sentence_length = NULL,
   output_column_name = 'kirk-bot',
-  count = 25,
+  count = 50,
   tries = 100,
   only_distinct = TRUE,
-  return_message = TRUE
-)
+  return_message = TRUE)
+  
 
--- src/get-tweets.R
+++ src/get-tweets.R
@@ -14,19 +14,27 @@ source("src/key-info.R")
 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
+options(width = 280)
+
 chucks_tweets <- userTimeline("@charliekirk11", n = 3200, includeRts=TRUE)
 chuck_df <- twListToDF(chucks_tweets) 
 
-chuck_df <- chuck_df %>%
+chuck_clean_df <- chuck_df %>%
   mutate(is_fakt = if_else(grepl("Fact", text), 1, 0),
-         is_link = if_else(grepl("http", text), 1, 0))
+         is_link = if_else(grepl("http", text), 1, 0),
+         new_text = gsub(" ?(f|ht)tp(s?)://(.*)", "", text),
+         new_text = gsub("\n\n", ". ", new_text))
 
-group_by(chuck_df, is_fakt) %>%
+urls <- str_extract(chuck_df$text, " ?(f|ht)tp(s?)://(.*)") %>%
+  str_trim() %>%
+  na.omit()
+
+group_by(chuck_clean_df, is_fakt) %>%
   summarize(n = n())
 
 ## Only 86 fakts...bummer. Nevertheless, I persisted.
 
-group_by(chuck_df, is_link) %>%
+group_by(chuck_clean_df, is_link) %>%
   summarize(n = n())
 
 ## He tweets a lot of linked bullshit
